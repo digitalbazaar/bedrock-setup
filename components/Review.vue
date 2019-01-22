ae4b99766aa341b8297513e2de7e77a42df35878
Manu Sporny <msporny@digitalbazaar.com>
2019-01-22 17:37:34 -0500

    Add Review component.

---
+++ components/Review.vue
@@ -0,0 +1,29 @@
+<template>
+  <dl class="horizontal">
+      <div v-for="item in config">
+        <dt>{{item.title}}</dt>
+        <dd v-if="!item.hidden">{{item.value}}</dd>
+        <dd v-else>
+          <q-input type="password" v-model="item.value"/>
+        </dd>
+      </div>
+  </dl>
+</template>
+<script>
+/*!
+ * Copyright (c) 2019 Digital Bazaar, Inc. All rights reserved.
+ */
+'use strict';
+
+import {required, email, helpers} from 'vuelidate/lib/validators';
+
+const strongPassword = helpers.regex('strongPassword',
+  /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.{16,})/);
+
+export default {
+  name: 'Review',
+  props: ['config']
+};
+</script>
+<style>
+</style>
