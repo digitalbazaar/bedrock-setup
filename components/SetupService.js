b665eab6565d64e8bc39ea1b1117a815ae0fe40a
Ganesh Annan <gannan@digitalbazaar.com>
2019-02-25 12:27:09 -0500

    Fix linting errors.

--- components/SetupService.js
+++ components/SetupService.js
@@ -18,7 +18,7 @@ export class SetupService {
   async get() {
     const url = this.config.urls.setup + '/';
     const response = await axios.get(url, {
-      headers: {'Accept': 'application/ld+json, application/json'}
+      headers: {Accept: 'application/ld+json, application/json'}
     });
     return response.data;
   }
